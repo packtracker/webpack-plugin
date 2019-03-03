608d7173b531b66cf51f27cac99ac62cf611741b
Jonathan Johnson <me@jondavidjohn.com>
2019-03-02 21:49:56 -0600

    fix up error handling

--- bin.js
+++ bin.js
@@ -23,10 +23,7 @@ const config = new Config({ upload: true, fail_build: true })
 const upload = new Upload(config)
 
 upload.process(stats, outputPath)
-  .then(() => {
-    console.log('Stats Uploaded!')
-    process.exit(0)
-  })
+  .then(() => process.exit(0))
   .catch((err) => {
     console.error('There was a problem uploading your stats.')
     console.error(err)
