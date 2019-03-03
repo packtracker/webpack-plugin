377bd54ca8c9e04cce47fa8e9d665b6b25bcbedd
Jonathan Johnson <me@jondavidjohn.com>
2019-03-02 21:48:28 -0600

    read stats file

--- bin.js
+++ bin.js
@@ -1,21 +1,34 @@
 #! /usr/bin/env node
 
 const args = require('minimist')(process.argv)
-const path = require('path')
+const { resolve, dirname } = require('path')
+const fs = require('fs')
 const Config = require('./config')
 const Upload = require('./upload')
 
-const statsJson = require(args.stats)
-const outputPath = args['output-path'] || path.dirname(args.stats)
+const statsFilePath = resolve(args['stats'])
+const outputPath = args['output-path'] || dirname(statsFilePath)
 
-const config = new Config({ fail_build: true })
+let stats
+
+try {
+  stats = JSON.parse(fs.readFileSync(statsFilePath, 'utf8'))
+} catch (err) {
+  console.error('There was a problem reading your stats file.')
+  console.error(err)
+  process.exit(1)
+}
+
+const config = new Config({ upload: true, fail_build: true })
 const upload = new Upload(config)
 
-upload.process(statsJson, outputPath)
+upload.process(stats, outputPath)
   .then(() => {
     console.log('Stats Uploaded!')
+    process.exit(0)
   })
   .catch((err) => {
     console.error('There was a problem uploading your stats.')
     console.error(err)
+    process.exit(1)
   })
